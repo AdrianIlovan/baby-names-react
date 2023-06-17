1802e031c5d28e39237a01df6dc9ef8405fd4cb3
Adrian Ilovan <adrian88ilovan@yahoo.com>
2023-06-17 15:00:52 +0100

    Updates L2

--- src/BabyNames.js
+++ src/BabyNames.js
@@ -1,13 +1,31 @@
 import React, { useState } from "react";
+import babyNamesData from './babyNamesData.json'
+
+
+const getFilteredItems = (search, items) => {
+    if (!search) {
+        return items
+    }
+    return items.filter(item => item.name.toLowerCase().includes(search.toLowerCase()));
+}
+
 
 const BabyNames = (props) => {
     const [search, setSearch] = useState("");
+    const filteredItems = getFilteredItems(search, babyNamesData);
 
     return(
         <div className="container">
+            <label>Search for baby names</label>
+            <input className="search-bar" 
+                type="text" 
+                placeholder="Search for a Name: "
+                value={search}
+                onChange={(e) => setSearch(e.target.props)}></input>
+            <div>
+                {filteredItems.map(item => <p key={item.name}>{item.name}</p>)}
+            </div>
 
-            <input className="container" type="text" placeholder="Search for a Name: "
-            onChange={e => setSearch(e.target.value)}></input>
             <div className="names">
             {props.babyNamesData.map((prop, key) => (
                 <div 
