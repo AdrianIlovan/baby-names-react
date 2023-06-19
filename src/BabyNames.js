8455d4e5c5006a53c180c7f1d578e91c85d4a2be
Adrian Ilovan <adrian88ilovan@yahoo.com>
2023-06-19 20:52:33 +0100

    Level 3 Done

--- src/BabyNames.js
+++ src/BabyNames.js
@@ -3,45 +3,59 @@ import babyNamesData from './babyNamesData.json';
 import Favorites from "./Favorites";
 
 const BabyNames = () => {
-    const [search, setSearch] = useState("");
-    const [favorites, setFavorites] = useState([]);
-  
+  const [search, setSearch] = useState("");
+  const [favorites, setFavorites] = useState([]);
+  const [filteredItems, setFilteredItems] = useState(babyNamesData);
+
+  const handleSearchChange = (e) => {
+    const searchValue = e.target.value;
+    setSearch(searchValue);
+
     const filteredItems = babyNamesData.filter(item =>
-      item.name.toLowerCase().includes(search.toLowerCase())
+      item.name.toLowerCase().includes(searchValue.toLowerCase())
     );
-    
-    const addToFavorites = (name) => {
-      setFavorites([...favorites, name]);
-    };
-    
-    return (
-      <div className="container">
-        
-        <input
-          className="search-bar"
-          type="text"
-          placeholder="Search for a Name:"
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-        ></input>
-        <div>
-          <Favorites favorites = {favorites}/>
-        </div>
-        <div className="names">
-          {filteredItems.map(item => (
-            <div
-              className={item.sex === "m" ? "male-names" : "female-names"}
-              key={item.id}
-              onClick={() => addToFavorites(item.name)}
-            >
-              <p>{item.name}</p>
-            </div>
-          ))}
-        </div>
-      
-      </div>
+    setFilteredItems(filteredItems);
+  };
+
+  const addToFavorites = (name) => {
+    setFavorites(prevFavorites => [...prevFavorites, name]);
+    setFilteredItems(prevFilteredItems =>
+      prevFilteredItems.filter(item => item.name !== name)
     );
   };
 
-  export default BabyNames;
-  
+  const removeFromFavorites = (name) => {
+    setFilteredItems(prevFilteredItems => [...prevFilteredItems, { name }]);
+    setFavorites(prevFavorites =>
+      prevFavorites.filter(favorite => favorite !== name)
+    );
+  };
+
+  return (
+    <div className="container">
+      <input
+        className="search-bar"
+        type="text"
+        placeholder="Search for a Name:"
+        value={search}
+        onChange={handleSearchChange}
+      />
+      <div className="favorites">
+        <Favorites favorites={favorites} removeFromFavorites={removeFromFavorites} addToFavorites={addToFavorites} />
+      </div>
+      <div className="names">
+        {filteredItems.map(item => (
+          <div
+            className={item.sex === "m" ? "male-names" : "female-names"}
+            key={item.id}
+            onClick={() => addToFavorites(item.name)}
+          >
+            <p>{item.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BabyNames;
--- src/Favorites.js
+++ src/Favorites.js
@@ -1,16 +1,26 @@
 import React from "react";
 
-const Favorites = ({favorites}) => {
-    return (
-        <div className="favorites">
-            <h1>Favorites: </h1>
-            <ul>
-                {favorites.map((name, index) => (
-                <div key={index}>{name}</div>
-                ))}
-            </ul>
+const Favorites = ({ favorites, removeFromFavorites, addToFavorites }) => {
+  return (
+    <div className="favorites">
+      <h1>Favorites:</h1>
+      {favorites.length > 0 ? (
+        <div className="favorite-names">
+          {favorites.map((name, index) => (
+            <div
+              key={index}
+              className={index % 2 === 0 ? "male-names" : "female-names"}
+              onClick={() => removeFromFavorites(name)}
+            >
+              <p>{name}</p>
+            </div>
+          ))}
         </div>
-    )
-}
+      ) : (
+        <p>No favorites selected yet.</p>
+      )}
+    </div>
+  );
+};
 
 export default Favorites;
